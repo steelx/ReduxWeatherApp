da8b829f3ee9b94f462ba46c44c48aab746f2b0d
Steel <ajinkya@riva.co>
2016-04-20 16:20:54 +0530

    new route

---
+++ src/views/BlogView/BlogView.js
@@ -0,0 +1,37 @@
+/* @flow */
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import SearchBar from '../../containers/search_bar';
+import WeatherList from '../../containers/weather_list';
+import classes from './BlogView.scss';
+
+
+// We avoid using the `@connect` decorator on the class definition so
+// that we can export the undecorated component for testing.
+// See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
+export class BlogView extends React.Component {
+
+  render() {
+    return (
+      <div className='container text-center'>
+        <div className='row'>
+          <div className='col-md-6 col-md-offset-2'>
+            <h3 className={classes.counter}>BlogView</h3>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-6 col-md-offset-2'>
+            <h1>List </h1>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  counter: state.counter
+});
+export default connect(mapStateToProps)(BlogView)
