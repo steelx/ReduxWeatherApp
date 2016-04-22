f0de59fe17cd095246125dcd7604e425c5b16fc1
Steel <ajinkya@riva.co>
2016-04-22 11:13:42 +0530

    redux-form validation

--- src/containers/posts_add.js
+++ src/containers/posts_add.js
@@ -17,20 +17,48 @@ class PostsAdd extends Component {
       <form onSubmit={handleSubmit(this.props.addPosts)}>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" placeholder="Title" {...title}/>
+          <input type="text" className={`form-control ${title.touched && title.invalid ? 'has-error' : '' }`} placeholder="Title" {...title}/>
+
+          <span className="text-help">
+            {title.touched ? title.error : ''}
+          </span>
         </div>
         <div className="form-group">
           <label>Post content</label>
-          <textarea className="form-control" {...content} />
+          <textarea className={`form-control ${content.touched && content.invalid ? 'has-error' : '' }`} {...content} />
+
+          <span className="text-help">
+            {content.touched ? content.error : ''}
+          </span>
         </div>
         <div className="form-group">
           <label>Email</label>
-          <input type="text" className="form-control" placeholder="categories" {...categories}/>
+          <input type="text" className={`form-control ${categories.touched && categories.invalid ? 'has-error' : '' }`} placeholder="categories" {...categories}/>
+
+          <span className="text-help">
+            {categories.touched ? categories.error : ''}
+          </span>
         </div>
         <button type="submit">Submit</button>
       </form>
     );
   }
+}
+
+function validation(values) {
+  const errors = {};
+
+  if(!values.title){
+    errors.title = 'Enter a title';
+  }
+  if(!values.content){
+    errors.content = 'Enter some content';
+  }
+  if(!values.categories){
+    errors.categories = 'Enter categories';
+  }
+
+  return errors;
 }
 
 function mapDispatchToProps(dispatch) {
@@ -42,7 +70,8 @@ function mapDispatchToProps(dispatch) {
 
 PostsAdd = reduxForm({
   form: 'PostsAddForm',
-  fields: ['title', 'content', 'categories']
+  fields: ['title', 'content', 'categories'],
+  validate: validation
 }, null, mapDispatchToProps)(PostsAdd);
 
 export default PostsAdd;
