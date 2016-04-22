a454a73e0f784c3aa35115fcab7fc490caae16b3
Steel <ajinkya@riva.co>
2016-04-22 18:21:42 +0530

    redux-auth WIP loginView

---
+++ src/views/LoginView/LoginView.js
@@ -0,0 +1,30 @@
+/* @flow */
+import React from 'react';
+import { OAuthSignInButton } from 'redux-auth';
+
+// We avoid using the `@connect` decorator on the class definition so
+// that we can export the undecorated component for testing.
+// See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
+export class LoginView extends React.Component {
+
+  render() {
+    return (
+      <div className='container text-center'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <h3>Login</h3>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-6'>
+            <OAuthSignInButton provider='github' />
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default LoginView;
