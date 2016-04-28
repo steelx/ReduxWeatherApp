97b0285cf50ad50fd4d71f510e2c1733dc9eba1d
Steel <ajinkya@riva.co>
2016-04-28 11:18:56 +0530

    axios post with url

--- src/redux/actions/action_github_signin.js
+++ src/redux/actions/action_github_signin.js
@@ -52,16 +52,11 @@ export function githubGeturi() {
 }
 
 export function githubSendCode(code) {
-  // const GITHUB_URL = `${ACCESS_TOKEN_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`;
+  const GITHUB_URL = `${ACCESS_TOKEN_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}`;
 
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   const axiosPost = axios.post(
     ACCESS_TOKEN_URL,
-    {
-      'client_id': CLIENT_ID,
-      'client_secret': CLIENT_SECRET,
-      'code': code
-    },
     {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
