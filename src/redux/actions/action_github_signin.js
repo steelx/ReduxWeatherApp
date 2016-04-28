7dea23f86f9c94ea770d5e0ae997bb396ee04066
Steel <ajinkya@riva.co>
2016-04-28 11:06:03 +0530

    axios accept header

--- src/redux/actions/action_github_signin.js
+++ src/redux/actions/action_github_signin.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { browserHistory } from 'react-router';
+import {browserHistory} from 'react-router';
 
 const CLIENT_ID = '32b70bf671e04762b26c';
 const CLIENT_SECRET = '5851623d94887db7612d4c9bc689310b9d53284b';
@@ -16,21 +16,21 @@ export const actionTypes = {
   SEND_CODE: 'SEND_CODE'
 };
 
-function signinRequest(){
+function signinRequest() {
   return {
     type: actionTypes.SIGNIN,
     payload: 'Github request sent'
   }
 }
 
-function signinUrl(payload){
+function signinUrl(payload) {
   return {
     type: actionTypes.SIGNIN_URL,
     payload
   }
 }
 
-function signinSuccess(payload){
+function signinSuccess(payload) {
   window.localStorage.setItem('token', payload.access_token);
   return {
     type: actionTypes.SIGNIN_SUCCESS,
@@ -38,25 +38,28 @@ function signinSuccess(payload){
   }
 }
 
-function signinError(payload){
+function signinError(payload) {
   return {
     type: actionTypes.SIGNIN_ERROR,
     payload
   }
 }
 
-export function githubGeturi(){
+export function githubGeturi() {
   const GITHUB_URL = `${AUTHORIZE_URL}?client_id=${CLIENT_ID}&scope=user,public_repo&redirect_uri=${REDIRECT_URL}`;
 
   return (dispatch) => dispatch(signinUrl(GITHUB_URL));
 }
 
-export function githubSendCode(code){
+export function githubSendCode(code) {
   const GITHUB_URL = `${ACCESS_TOKEN_URL}?client_id=${CLIENT_ID}&redirect_uri=${ROOT_URL}&client_secret=${CLIENT_SECRET}&code=${code}`;
 
   axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-  const axiosPost = axios.post(GITHUB_URL);
+  const axiosPost = axios.post(GITHUB_URL, {
+    headers: {
+      'Accept': 'text/json'
+    }
+  });
 
   //headers: {'X-Requested-With': 'XMLHttpRequest'},
 
